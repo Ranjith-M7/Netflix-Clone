1e4037611a16b11b6f2ef2854d2b0d553235f23f
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-10-01 15:09:17 +0530

    updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -10,8 +10,26 @@ const firebaseConfig = {
   projectId: "netflix-clone-d2a40",
   storageBucket: "netflix-clone-d2a40.appspot.com",
   messagingSenderId: "1027186254910",
-  appId: "1:1027186254910:web:a637b21307881d6b405d6f"
+  appId: "1:1027186254910:web:a637b21307881d6b405d6f",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signUp = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+
+    await addDoc(collection(db, "user"), {
+      uid: user.id,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
