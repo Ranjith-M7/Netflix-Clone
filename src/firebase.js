665949028908693caddf444c56e161c47e5f2eef
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-10-23 12:24:30 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -21,7 +21,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-console.log(import.meta.env);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
